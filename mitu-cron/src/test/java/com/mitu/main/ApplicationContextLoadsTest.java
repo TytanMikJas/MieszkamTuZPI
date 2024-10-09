101153e6e26152e6bec68b978726bbb7abf6f65a
wwiktor <266597@student.pwr.edu.pl>
2024-10-09 18:50:58 +0200

    MITU-149: Setup barebone mitu-cron module

---
+++ mitu-cron/src/test/java/com/mitu/main/ApplicationContextLoadsTest.java
@@ -0,0 +1,13 @@
+package com.mitu.main;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ApplicationContextLoadsTest {
+    @Test
+    public void contextLoads() {}
+}
