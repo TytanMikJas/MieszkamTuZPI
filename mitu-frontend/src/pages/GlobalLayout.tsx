d050ceeb26499937cb56f95169c0869881cfab57
TytanMikJas <mikjas02@gmail.com>
2024-10-09 19:18:41 +0200

    MITU-178 Duplikaty w package-lock

--- mitu-frontend/src/pages/GlobalLayout.tsx
+++ mitu-frontend/src/pages/GlobalLayout.tsx
@@ -1,10 +1,7 @@
-import Navbar from '@/reusable-components/navbar/Navbar';
 import { Toaster } from 'sonner';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { memo, useEffect } from 'react';
 import { Outlet } from 'react-router';
-import ImageGallery from '@/reusable-components/ImageGallery/ImageGallery';
-import ForceResetPasswordContainer from '@/reusable-components/reset-password/ForceResetPasswordContainer';
 
 const GlobalLayout = function () {
   return (
