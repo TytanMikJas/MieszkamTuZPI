aca41a45f654e1be34c5be5456c7a8f543b74c3d
MikJasTytan <mikjas02@gmail.com>
2024-10-12 01:24:40 +0200

    Utworzenie schema bazy danych na backendzie nest + zmiany Dcoker-compose.yml

---
+++ mitu-devops/build_and_push_tags.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal
+
+:: Generate a unique signal name
+set SIGNAL=BuildSignal%random%
+
+:: Start backend build and push in the background
+start /b cmd /c "(cd .. && docker build -t registry.gitlab.com/mieszamtu/mieszkamtuzpi/mitu_backend:latest -f backend.Dockerfile . && waitfor /si %SIGNAL%)"
+
+:: Start frontend build and push in the background
+start /b cmd /c "(cd .. && docker build -t registry.gitlab.com/mieszamtu/mieszkamtuzpi/mitu_frontend:latest -f frontend.Dockerfile . && waitfor /si %SIGNAL%)"
+
+:: Start frontend build and push in the background
+start /b cmd /c "(cd .. && docker build -t registry.gitlab.com/mieszamtu/mieszkamtuzpi/mitu_cron:latest -f cron.Dockerfile . && waitfor /si %SIGNAL%)"
+
+:: Wait for both commands to signal completion
+waitfor %SIGNAL% /t 3600
+waitfor %SIGNAL% /t 3600
+waitfor %SIGNAL% /t 3600
+
+echo All 3 Docker builds have been completed.
+endlocal
