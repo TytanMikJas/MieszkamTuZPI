a5e6062a4e716c7e561180f8564ec4e54970e7a2
Mikołaj Jastrzębski <266858@student.pwr.edu.pl>
2024-10-15 16:19:44 +0000

    MITU-181 Dodać "unused imports" jako błąd w konfiguracji eslinta

--- mitu-frontend/src/core/api/error-utils.ts
+++ mitu-frontend/src/core/api/error-utils.ts
@@ -3,8 +3,8 @@ import { AxiosError } from 'axios';
 import { ERROR_DISPLAY_ALERT } from '@/strings';
 import { ErrorIncomingDto } from './common/error/ErrorDto';
 
-
-export function handleErrorResponse(error: AxiosError<{data: any}> ): any[] { //TODO remove any
+export function handleErrorResponse(error: AxiosError<{ data: any }>): any[] {
+  //TODO remove any
   if (!error.response || !error.response.data || !error.response.data.data) {
     return [];
   }
@@ -26,7 +26,7 @@ export function handleErrorResponse(error: AxiosError<{data: any}> ): any[] { //
     .reduce((acc: any, val: any[]) => acc.concat(val), []);
 }
 
-export function emitErrors(error: AxiosError<{data: any}>): void {
+export function emitErrors(error: AxiosError<{ data: any }>): void {
   const errors = handleErrorResponse(error);
   errors.forEach((error) => {
     if (error.type === ERROR_DISPLAY_ALERT) {
