7dd58ea8f932d9c5805ca26d8f25f1e8e1f8f40e
Damian Sawko <266854@student.pwr.edu.pl>
2024-11-05 17:44:44 +0000

    MITU-70 "Seo"

--- mitu-devops/build_and_push_tags.bat
+++ mitu-devops/build_and_push_tags.bat
@@ -10,6 +10,9 @@ start /b cmd /c "(cd .. && docker build -t registry.gitlab.com/mieszamtu/mieszka
 :: Start frontend build and push in the background
 start /b cmd /c "(cd .. && docker build -t registry.gitlab.com/mieszamtu/mieszkamtuzpi/mitu_frontend:latest -f frontend.Dockerfile . && waitfor /si %SIGNAL%)"
 
+:: Start prerender build and push in the background
+start /b cmd /c "(cd .. && docker build -t registry.gitlab.com/mieszamtu/mieszkamtuzpi/mitu_prerender:latest -f prerender.Dockerfile . && waitfor /si %SIGNAL%)"
+
 :: Start frontend build and push in the background
 start /b cmd /c "(cd .. && docker build -t registry.gitlab.com/mieszamtu/mieszkamtuzpi/mitu_cron:latest -f cron.Dockerfile . && waitfor /si %SIGNAL%)"
 
@@ -17,6 +20,7 @@ start /b cmd /c "(cd .. && docker build -t registry.gitlab.com/mieszamtu/mieszka
 waitfor %SIGNAL% /t 3600
 waitfor %SIGNAL% /t 3600
 waitfor %SIGNAL% /t 3600
+waitfor %SIGNAL% /t 3600
 
-echo All 3 Docker builds have been completed.
+echo All 4 Docker builds have been completed.
 endlocal
---
+++ mitu-prerender/README.md
@@ -0,0 +1,20 @@
+## Prerender
+
+### Testing
+
+Emulate a normal user request:
+TODO add correct url
+
+```bash
+curl -A Chrome localhost/mapa/start
+curl -A Chrome https://url.mieszkamtu.pl/mapa/start
+```
+
+Emulate a crawler request:
+TODO add correct url
+
+```bash
+curl -A Googlebot localhost/mapa/start
+curl -A Googlebot https://url.mieszkamtu.pl/mapa/start
+
+```
